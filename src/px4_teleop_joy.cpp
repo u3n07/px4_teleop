d88478ba1447111894da7c6116a08785d5d9ce76
Takaki Ueno <t_ueno@eis.hokudai.ac.jp>
2017-08-17 11:55:58 +0900

    Add px4_teleop_joy node

---
+++ src/px4_teleop_joy.cpp
@@ -0,0 +1,19 @@
+// C/C++ Libraries
+#include <iostream>
+
+// roscpp
+#include <ros/ros.h>
+
+// geometry_msgs
+#include <geometry_msgs/TwistStamped.h>
+
+// mavros_msgs
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+#include <mavros_msgs/CommandTOL.h>
+#include <mavros_msgs/CommandHome.h>
+
+int main(int argc, char **argv){
+  return 0;
+}
